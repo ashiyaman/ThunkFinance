be8e2306449e470f12c035be0aadc42b9a18f5a3
ashiyaman <ashiya1005@gmail.com>
2025-02-24 11:27:32 +0530

    chore: Add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 
+import IncomeExpenseForm from './incomeExpenseForm'
+
 function App() {
 
   return (
-    <></>
+    <>
+      <IncomeExpenseForm />
+    </>
   )
 }
 
